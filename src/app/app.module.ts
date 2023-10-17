3593904230390a7079c8761c2e593b2b1de5c964
Shilpaheidi <shilpan048@gmail.com>
2023-10-17 11:38:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { HomeComponent } from './home/home.component';
+import { MyCasesComponent } from './my-cases/my-cases.component';
+import { ContentComponent } from './content/content.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    HomeComponent,
+    MyCasesComponent,
+    ContentComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
